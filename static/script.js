d925c4a23b24853800f7b0b1939ae6423a92796f
Katya Katsy <kkatsy@ucdavis.edu>
2021-02-16 12:59:49 -0800

    table clears

--- static/script.js
+++ static/script.js
@@ -24,6 +24,7 @@ function clearForm() {
   document.getElementById("result").innerHTML = "";
   $(":checkbox").prop('checked', false).parent().removeClass('active');
 
+  $("#dependencyTable tr").remove()
 }
 
 
--- twitter.py
+++ twitter.py
@@ -1,9 +1,9 @@
 import re
 import tweepy
 from tweepy import OAuthHandler
-from textblob import TextBlob
 
 LIMIT = 200
+
 
 class Twitter(object):
     """
@@ -26,54 +26,10 @@ class Twitter(object):
         # create tweepy API object to fetch tweets
         self.api = tweepy.API(self.auth)
 
-    def clean_tweet(self, tweet):
-        # remove special chars, links, and @usernames
-        return ' '.join(re.sub("(@[A-Za-z0-9]+)|([^0-9A-Za-z \t]) | (\w+:\ / \ / \S+) ", " ", tweet).split())
-
-    def get_sentiment(self, tweet):
-        # get sentiment index between -1 and 1
-        blob = TextBlob(tweet)
-        polarity = blob.sentiment.polarity
-
-        # get sentiment
-        if polarity > 0.0:
-            sentiment = 'positive'
-        elif polarity == 0.0:
-            sentiment = 'neutral'
-        else:
-            sentiment = 'negative'
-
-        return sentiment, polarity
-
-    def get_data(self, tweet):
-        # dict of tweet data
-        tweet_data = {}
-
-        # get full text of tweet
-        if 'RT ' in tweet.full_text[0:4]:
-            tweet_data['text'] = tweet.retweeted_status.full_text
-        else:
-            tweet_data['text'] = tweet.full_text
-
-        tweet_data['text'] = re.sub(r'(https|http)?:\/\/(\w|\.|\/|\?|\=|\&|\%)*\b', '', tweet_data['text'], flags=re.MULTILINE)
-
-        # get list of separated words in tweet
-        tweet_data['word_list'] = re.sub("(@[A-Za-z0-9]+)|([^0-9A-Za-z \t]) | (\w+:\ / \ / \S+) ", " ",
-                                         tweet_data['text']).lower().split()
-        special_free = [word for word in tweet_data['word_list'] if word.isalnum()]
-        tweet_data['word_list'] = special_free
-
-        # get language to make sure can analyze english-only
-        tweet_data['lang'] = tweet.lang
-
-        # get sentiment of tweet
-        tweet_data['sentiment'], tweet_data['polarity'] = self.get_sentiment(' '.join(tweet_data['word_list']))
-
-        return tweet_data
 
     def get_tweets(self, query, count):
         # store processed tweet data dicts in list
-        processed_tweets = []
+        tweet_id_num_batch = []
         calls = 0
 
         while count > 0:
@@ -88,9 +44,12 @@ class Twitter(object):
             calls += 1
 
             # for tweets in batch, get full text and process tweets
-            tweet_batch = []
             for single_tweet in single_call:
-                full_tweet = self.api.get_status(single_tweet.id, tweet_mode='extended')
-                tweet_batch.append(self.get_data(full_tweet))
-            processed_tweets.extend(tweet_batch)
-        return processed_tweets
+                tweet_id = single_tweet.id
+                tweet_id_num_batch.append(tweet_id)
+
+        return tweet_id_num_batch
+
+# twitter = Twitter()
+# ids = twitter.get_tweets('hacker', 20)
+# print(ids)
