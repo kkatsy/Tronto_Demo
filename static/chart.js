b324bf7583b02cda28a4e7db627ce67bea946770
Katya Katsy <kkatsy@ucdavis.edu>
2021-04-05 20:58:48 -0700

    chatbot css okay-ish

--- static/chart.js
+++ static/chart.js
@@ -25,7 +25,7 @@ function addPieChart2(){
         labels: ["Africa", "Asia", "Europe", "Latin America", "North America"],
         datasets: [{
           label: "Population (millions)",
-          backgroundColor: ["rgba(84,161,229,1.0)","rgba(108,190,191,1.0)","rgba(248,206,107,1.0)","rgba(242,162,84,1.0)","rgba(237,110,133,1.0)"],
+          backgroundColor: ["rgba(255,232,161,1.0)","rgba(180,71,84,1.0)","rgba(248,206,107,1.0)","rgba(242,162,84,1.0)","rgba(237,110,133,1.0)"],
           data: [343,768,235,666,478]
         }]
       },
