449f8558891329aa378d56119d569fb63135e070
Katya Katsy <kkatsy@ucdavis.edu>
2021-10-01 13:24:16 -0700

    working local api

--- app.py
+++ app.py
@@ -14,33 +14,32 @@ def home():
 
 
 # route to tweet pipeline server
-@app.route('/tweet_server/<json_str>',methods=['GET'])
+@app.route('/tweet_server/<json_str>',methods=['POST'])
 def tweet_server(json_str):
-
     # get json app + dependencies from JS
     json_str = unquote(json_str)
-
+    print("json_str", json_str)
     tweet_dict = json.loads(json_str)
-
+    print("tweet_dict", tweet_dict)
     sorted_tweet_dict = sort_tweets(tweet_dict)
-
+    print("sorted_tweet_dict", sorted_tweet_dict)
     return json.dumps(sorted_tweet_dict)
 
 
 # route to question answering server
-@app.route('/qa_server/<json_str>',methods=['GET'])
+@app.route('/qa_server/<json_str>',methods=['POST'])
 def qa_server(json_str):
 
     # get json app + dependencies from JS
     json_str = unquote(json_str)
-
+    print("json_str", json_str)
     app_dict = json.loads(json_str)
-
+    print("app_dict", app_dict)
     app_data_dict = answer_q(app_dict)
-
+    print("app_data_dict", app_data_dict)
     return json.dumps(app_data_dict)
 
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(host= "0.0.0.0", port=9080, debug=True)
